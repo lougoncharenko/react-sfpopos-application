9f014f892b19eca4dd6715c3dab5c1955fc7a769
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-07 12:50:10 -0500

    saving progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,12 @@
+import '../styles/title.css'
+
 function Title() {
     return (
-      <div>
-        <h1>SFPOPOS</h1>
+      <div className='Title'>
+        <h1 >SFPOPOS</h1>
+        <div className="Title-Subtitle">
+            San Franciscos Privately Owned Public Spaces
+        </div>
       </div>
     )
   }
