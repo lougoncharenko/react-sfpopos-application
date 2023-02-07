0e3e2371441ed8fe955ec738616638eeac930da4
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-02-07 12:56:58 -0500

    created a card for spaces

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Title from './components/Title';
+import POPOSList from './components/POPOSList';
 
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
---
+++ src/components/POPOSSpace.js
@@ -0,0 +1,11 @@
+function POPOSSpace() {
+    return (
+      <div>
+        <img src= "https://wp-tid.zillowstatic.com/bedrock/app/uploads/sites/5/2022/08/ZG_ZPA_08_21_Americana_ExtFront_1081_m2484px-copy_mobileImg2x-2.jpg" width="300" height="300" alt="Hello" />
+        <h1>Name...</h1>
+        <div>Address...</div>
+      </div>
+    )
+  }
+  
+  export default POPOSSpace
