7761c4ced300af37d6f1bf477b8b826a9b96b22c
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-04-10 20:23:32 -0400

    finished form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Form from "../components/Form/Form";
 
 function Newsletter() {
  
       return (
         <div >
             <h1>Sign up for the newsletter</h1>
+            <Form />
         </div>
       )
     }
