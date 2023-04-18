1bec1df9ace521865ef4c64179b2277a8943df7a
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-04-18 13:43:30 -0400

    added ARIA roles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,72 +9,72 @@ const Footer = () => {
       <Container>
         <Row className='footer-row'>
           <Col className="footer-section" lg="4">
-          <section className="logo">
+          <div className="logo" role="region" >
               <img id="logo" 
               src="https://img.freepik.com/premium-vector/house-real-estate-logo_7169-95.jpg" 
               alt="elite home official logo" />
-              <section>
+              <div role="region">
                 <h2 className="footer-heading">Elite Homes</h2>
-              </section>
-            </section>
+              </div>
+            </div>
             <p className="footer_text mt-4">
               Find your perfect forever home </p>
               <p className="footer_text mt-4">in the San Francisco area with </p>
               <p className="footer_text mt-4">Elite Homes!</p>
           </Col>
           <Col className="footer-section" lg="3">
-            <section className="footer_quick-links">
+            <div className="footer_quick-links" role="listitem">
               <h2 className="quick_links-title">Top Homes</h2>
               <ListGroup >
-              <section className='link_list_item'>
+              <div className='link_list_item' role="listitem">
                   <Link to='#'>1 Bedroom Homes</Link>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <Link to='#'>2 Bedroom Homes</Link>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <Link to='#'>3 Bedroom Home</Link>
-                </section>
+                </div>
               </ListGroup>
-            </section>
+            </div>
           </Col>
           <Col className="footer-section" lg="2">
-          <section className="footer_quick-links">
+          <div className="footer_quick-links" role="listitem">
               <h2 className="quick_links-title">Useful Links</h2>
               <ListGroup >
-                <section className='link_list_item'>
+                <div className='link_list_item' role="listitem">
                   <Link to='/shop'>About</Link>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <Link to='/cart'>NewsLetter</Link>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <Link to='/login'>Login</Link>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <Link to='/privacy'>Privacy</Link>
-                </section>
+                </div>
               </ListGroup>
-            </section>
+            </div>
           </Col>
           <Col className="footer-section" lg="3">
-          <section className="footer_quick-links">
+          <div className="footer_quick-links" role="region">
               <h2 className="quick_links-title">Contact Us</h2>
               <ListGroup >
-                <section className='link_list_item'>
+                <div className='link_list_item' role="listitem">
                   <span className='icons'><i class="ri-map-pin-line"></i></span>
                   <p> 345 14th street, San Francisco CA 94016</p>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <span className='icons'><i class="ri-phone-line"></i></span>
                   <p>+4155761848</p>
-                </section>
-                <section className='link_list_item'>
+                </div>
+                <div className='link_list_item' role="listitem">
                   <span className='icons'><i class="ri-mail-line"></i></span>
                   <p>elitehome-help@gmail.com</p> 
-                </section>
+                </div>
               </ListGroup>
-            </section>
+            </div>
           </Col>
         
         </Row>
